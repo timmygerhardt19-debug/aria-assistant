7af9a60be4c4873f08ab96327072b85dd9fc5fc5
timmygerhardt19-debug <timmy.gerhardt19@gmail.com>
2026-03-20 17:20:21 -0600

    Update server.js

--- server.js
+++ server.js
@@ -67,5 +67,6 @@ cron.schedule('* * * * *', () => {
   });
 });
 
-app.listen(3000, () => console.log('ARIA is running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('ARIA is running on port ' + PORT));
 ```
